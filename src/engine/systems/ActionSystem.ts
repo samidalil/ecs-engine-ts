718007e56edc7c32e76239124d6f5b43631d099e
IceBirdCiel <cceline74@hotmail.fr>
2021-07-29 13:25:23 +0200

    change Physics

--- src/engine/systems/ActionSystem.ts
+++ src/engine/systems/ActionSystem.ts
@@ -1,28 +1,33 @@
-import { ActionComponent, PhysicsComponent } from "../components";
+import { Vector3 } from "../../lib/math";
+import { ActionComponent, PhysicsComponent, TransformComponent } from "../components";
 import { Action } from "../components/ActionComponent";
 import { Components } from "../components/types";
 import Entity from "../Entity";
 import BaseSystem from "./BaseSystem";
 
-class ActionSystem extends BaseSystem<[ActionComponent, PhysicsComponent]> {
-  public readonly componentTypeArray = [Components.Action, Components.Physics];
+class ActionSystem extends BaseSystem<[ActionComponent, PhysicsComponent, TransformComponent]> {
+  public readonly componentTypeArray = [Components.Action, Components.Physics, Components.Transform];
 
   public behaviour = (
     entity: Entity,
-    [action, physics]: [ActionComponent, PhysicsComponent]
+    [action, physics, transform]: [ActionComponent, PhysicsComponent, TransformComponent]
   ) => {
     switch (action.consume()) {
       case Action.MOVE_LEFT:
-        physics.velocity.x -= 2;
+        //physics.velocity.x -= 2;
+        transform.rotation.y -= 1;
         break;
       case Action.MOVE_RIGHT:
-        physics.velocity.x += 2;
+        //physics.velocity.x += 2;np
+        transform.rotation.y += 1;
         break;
       case Action.MOVE_BACKWARD:
-        physics.velocity.z -= 2;
+        //physics.velocity.z -= 0.2;
+        transform.position.z -= 0.2;
         break;
       case Action.MOVE_FORWARD:
-        physics.velocity.z += 2;
+        //physics.velocity.z += 0.2;
+        transform.position.z += 0.2;
         break;
       case Action.JUMP:
         if (physics.velocity.y === 0)
